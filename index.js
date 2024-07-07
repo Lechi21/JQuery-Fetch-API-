c068dc512129efd67d8a4e2b31ce31334c78f504
Kelechi Gift <112638489+Lechi21@users.noreply.github.com>
2024-07-07 18:21:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -14,13 +14,13 @@ $(document).keypress(function(event){
 
 //=====================FETCH SECTION=============================//
 
-const name = $('#name')
-const eye_color = $('#eye_color')
-const birthyear = $('#birthyear')
-const height = $('#height');
-const randomer = $('#randomer');
+const name = document.getElementById('name')
+const eye_color = document.getElementById('eye_color')
+const birthyear = document.getElementById('birthyear')
+const height = document.getElementById('height');
+const randomer = document.getElementById('randomer');
 
-const button = $('.change')
+const button = document.querySelector('.change')
 
 
 button.addEventListener('click', (e) =>{
